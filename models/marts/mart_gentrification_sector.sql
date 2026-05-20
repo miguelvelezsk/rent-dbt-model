4ead924af5758c2cbff0166ac4be5a607da123d2
Miguelangel Velez Aguirre <miguelvelezaguirre10@gmail.com>
2026-05-20 22:46:02 +0000

    feature: add inflation calculations

--- models/marts/mart_gentrification_sector.sql
+++ models/marts/mart_gentrification_sector.sql
@@ -7,7 +7,11 @@ SELECT
     COUNT(*) AS total_ofertas,
     ROUND(AVG(precio_mensual), 2) AS precio_promedio_sector,
     ROUND(AVG(area_m2), 2) AS area_promedio_sector,
-    ROUND(AVG(precio_por_m2), 2) AS costo_m2_promedio
+    ROUND(AVG(precio_por_m2), 2) AS costo_m2_promedio,
+    ROUND(AVG(precio_esperado_inflacion), 2) AS costo_m2_esperado_inflacion,
+    ROUND(AVG(brecha_inflacion_pct), 2) AS porcentaje_sobrecosto_mercado,
+    ROUND(AVG(brecha_inflacion_renta_pct), 2) AS porcentaje_sobrecosto_renta_mensual,
+    ROUND(AVG(precio_esperado_renta_mensual), 2) AS costo_renta_mensual_esperado_inflacion
 FROM rentals
 GROUP BY barrio
 HAVING total_ofertas >= 3  
--- models/staging/stg_rentals.sql
+++ models/staging/stg_rentals.sql
@@ -9,6 +9,10 @@ SELECT
     CAST(price AS INT64) AS precio_mensual,
     CAST(total_area_m2 AS INT64) AS area_m2,
     CAST(price_per_m2 AS FLOAT64) AS precio_por_m2,
+    CAST(expected_price_per_m2 AS FLOAT64) AS precio_esperado_inflacion,
+    CAST(gentrification_gap_pct AS FLOAT64) AS brecha_inflacion_pct,
+    CAST(expected_monthly_rent AS INT64) AS precio_esperado_renta_mensual,
+    CAST(gentrification_rental_gap_pct AS FLOAT64) AS brecha_inflacion_renta_pct,
     bathrooms AS banos,
     bedrooms AS habitaciones
 FROM source
